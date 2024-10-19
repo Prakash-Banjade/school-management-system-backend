e2e68f28e614dc12587e0ed41b5f54cffe9881a9
Prakash Banjade <prakashbanjade191@gmail.com>
2024-10-19 15:08:44 +0545

    use validation schema for environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,13 @@ import { MailModule } from './mail/mail.module';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AuthGuard } from './common/guards/auth.guard';
+import { envSchema } from './env.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: envSchema,
     }),
     NestjsFormDataModule.config({
       storage: MemoryStoredFile,
