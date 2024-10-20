8375dc701df49166767f7c98955025519d514b1e
Prakash Banjade <prakashbanjade191@gmail.com>
2024-10-20 21:22:38 +0545

    default order set to DESC

--- src/common/dto/pageOptions.dto.ts
+++ src/common/dto/pageOptions.dto.ts
@@ -8,10 +8,10 @@ enum Order {
 }
 
 export class PageOptionsDto {
-    @ApiPropertyOptional({ enum: Order, default: Order.ASC })
+    @ApiPropertyOptional({ enum: Order, default: Order.DESC })
     @IsEnum(Order)
     @IsOptional()
-    readonly order?: Order = Order.ASC;
+    readonly order?: Order = Order.DESC;
 
     @ApiPropertyOptional({
         minimum: 1,
