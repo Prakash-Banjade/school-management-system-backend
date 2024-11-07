a5619ff31e8f549857417bdd4bb7389e88570990
Prakash Banjade <prakashbanjade191@gmail.com>
2024-11-07 12:10:49 +0545

    import module

--- src/_role-based-view/student/tasks/tasks.module.ts
+++ src/_role-based-view/student/tasks/tasks.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { Task } from 'src/task-system/tasks/entities/task.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Task]),
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })
-export class TasksModule {}
+export class TasksModule { }
