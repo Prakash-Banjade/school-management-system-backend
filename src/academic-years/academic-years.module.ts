700b69d2978df04ec740387befbea873627b400a
Prakash Banjade <prakashbanjade191@gmail.com>
2024-11-17 16:03:45 +0545

    make module global

--- src/academic-years/academic-years.module.ts
+++ src/academic-years/academic-years.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AcademicYearsService } from './academic-years.service';
 import { AcademicYearsController } from './academic-years.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AcademicYear } from './entities/academic-year.entity';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forFeature([AcademicYear]),
