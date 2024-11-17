dd8bd78387e02e39b404240220a5debc7f376bcb
Prakash Banjade <prakashbanjade191@gmail.com>
2024-11-17 15:30:00 +0545

    fix: failed subjects count

--- src/examination-system/exams/helpers/exams.helper.ts
+++ src/examination-system/exams/helpers/exams.helper.ts
@@ -144,7 +144,7 @@ export class ExamsHelper extends BaseRepository {
         const { gpa, grade } = await this.examReportsService.getGpaAndGrade(percentage);
 
         const failedSubjectsCount = exam.examSubjects?.filter(examSubject => (
-            (examSubject.examReports[0]?.theoryOM + examSubject.examReports[0]?.practicalOM) < (examSubject.theoryPM + examSubject.practicalPM)
+            (examSubject.examReports[0]?.theoryOM < examSubject.theoryPM) || (examSubject.examReports[0]?.practicalOM < examSubject.practicalPM)
         )).length;
 
         const weakestSubject = exam.examSubjects?.sort((a, b) => a.examReports[0]?.percentage - b.examReports[0]?.percentage)[0]?.subject?.subjectName;
