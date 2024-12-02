aa1e636cff40532eb65a16b6260e4182c291c8cc
Prakash Banjade <prakashbanjade191@gmail.com>
2024-12-02 19:18:30 +0545

    change timezone

--- src/datasource/typeorm.module.ts
+++ src/datasource/typeorm.module.ts
@@ -16,7 +16,7 @@ import { Global, Module } from '@nestjs/common';
                         url: process.env.DATABASE_URL,
                         entities: [`${__dirname}/../**/**.entity{.ts,.js}`], // this will automatically load all entity file in the src folder
                         synchronize: process.env.DB_SYNCHRONIZE === 'true',
-                        timezone: '+00:00', // Use UTC
+                        timezone: '+05:45', // Use UTC
                     });
                     await dataSource.initialize(); // initialize the data source
                     console.log('Database connected successfully');
