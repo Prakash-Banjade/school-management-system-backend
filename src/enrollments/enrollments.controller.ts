61eb267b19808eb0a6c0d941b8ff4320731ec7c4
Prakash Banjade <prakashbanjade191@gmail.com>
2024-12-03 13:56:49 +0545

    add api tag

--- src/enrollments/enrollments.controller.ts
+++ src/enrollments/enrollments.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, UseInterceptors } from '@nestjs/common';
 import { EnrollmentsService } from './enrollments.service';
 import { CreateEnrollmentDto } from './dto/create-enrollment.dto';
 import { EnrollmentQueryDto } from './dto/enrollment-query.dto';
 import { TransactionInterceptor } from 'src/common/interceptors/transaction.interceptor';
 import { CheckAbilities } from 'src/common/decorators/abilities.decorator';
 import { Action, Role } from 'src/common/types/global.type';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Enrollments')
 @Controller('enrollments')
 export class EnrollmentsController {
   constructor(private readonly enrollmentsService: EnrollmentsService) { }
@@ -23,20 +26,4 @@ export class EnrollmentsController {
   @CheckAbilities({ subject: Role.ADMIN, action: Action.READ }) findAll(@Query() queryDto: EnrollmentQueryDto) {
     return this.enrollmentsService.findAll(queryDto);
   }
-
-  // @Get(':id')
-  // @CheckAbilities({ subject: Role.ADMIN, action: Action.READ })
-  // findOne(@Param('id') id: string) {
-  //   return this.enrollmentsService.findOne(id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateEnrollmentDto: UpdateEnrollmentDto) {
-  //   return this.enrollmentsService.update(+id, updateEnrollmentDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.enrollmentsService.remove(+id);
-  // }
 }
