11a9a85402b5287e9a524ac6047a063933088160
Prakash Banjade <prakashbanjade191@gmail.com>
2024-12-24 15:50:12 +0545

    feat: implement dashboard branch wise

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,71 +1,88 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Scope } from '@nestjs/common';
 import { BaseRepository } from 'src/common/repository/base-repository';
-import { DataSource } from 'typeorm';
+import { Brackets, DataSource } from 'typeorm';
 import { FastifyRequest } from 'fastify';
 import { REQUEST } from '@nestjs/core';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
 import { Student } from 'src/students/entities/student.entity';
-import { CACHE_KEYS } from 'src/common/CONSTANTS';
 import { Teacher } from 'src/teachers/entities/teacher.entity';
 import { ClassRoom } from 'src/class-rooms/entities/class-room.entity';
 import { Staff } from 'src/staffs/entities/staff.entity';
 import { LeaveRequest } from 'src/leave-requests/entities/leave-request.entity';
 import { EClassType, ELeaveRequestStatus, Role } from 'src/common/types/global.type';
+import { UtilitiesService } from 'src/utilities/utilities.service';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class DashboardService extends BaseRepository {
     constructor(
         datasource: DataSource, @Inject(REQUEST) req: FastifyRequest,
-        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+        private readonly utilitiesService: UtilitiesService,
     ) { super(datasource, req); }
 
     async getAdminDashboardCounts() {
-        const currentAcademicYearId = await this.cacheManager.get(CACHE_KEYS.CAY_ID);
+        const currentAcademicYearId = await this.utilitiesService.getAcademicYearId();
+        const branchId = this.utilitiesService.getBranchId();
 
-        const studentsCount = await this.getRepository(Student).createQueryBuilder('student')
-            .leftJoin('student.enrollments', 'enrollments')
-            .where('enrollments.academicYearId = :academicYearId', { academicYearId: currentAcademicYearId })
-            .getCount();
+        const studentsCountQueryBuilder = this.getRepository(Student).createQueryBuilder('student')
+            .leftJoin('student.account', 'account')
+            .leftJoin('student.enrollments', 'enrollments', 'enrollments.academicYearId = :academicYearId', { academicYearId: currentAcademicYearId })
+            .where(new Brackets(qb => {
+                qb.where('enrollments.id IS NOT NULL')
+                branchId && qb.andWhere('account.branchId = :branchId', { branchId })
+            }))
+        this.utilitiesService.applyBranchFilter(studentsCountQueryBuilder);
 
-        const teachersCount = await this.getRepository(Teacher).createQueryBuilder().getCount();
+        const teachersCountQueryBuilder = this.getRepository(Teacher).createQueryBuilder('teacher')
+            .leftJoin('teacher.account', 'account')
+            .where(new Brackets(qb => {
+                branchId && qb.andWhere('account.branchId = :branchId', { branchId })
+            }))
+        this.utilitiesService.applyBranchFilter(teachersCountQueryBuilder);
 
-        const classRoomsCount: {
-            sectionsCount: string,
-            primaryWithNoSectionsCount: string,
-        } = await this.getRepository(ClassRoom).createQueryBuilder('classRoom')
+        const staffsCountQueryBuilder = this.getRepository(Staff).createQueryBuilder('staff')
+            .leftJoin('staff.account', 'account')
+            .where(new Brackets(qb => {
+                branchId && qb.andWhere('account.branchId = :branchId', { branchId })
+            }))
+        this.utilitiesService.applyBranchFilter(staffsCountQueryBuilder);
+
+        const classRoomsCountQueryBuilder = this.getRepository(ClassRoom).createQueryBuilder('classRoom')
             .leftJoin('classRoom.children', 'children')
             .select([
                 'COUNT(DISTINCT CASE WHEN classRoom.classType = :primary THEN children.id END) as sectionsCount',
                 'COUNT(DISTINCT CASE WHEN classRoom.classType = :primary AND children.id IS NULL THEN classRoom.id END) as primaryWithNoSectionsCount',
             ])
             .setParameter('primary', EClassType.PRIMARY)
-            .getRawOne();
 
-        const staffsCount = await this.getRepository(Staff).createQueryBuilder().getCount();
+        const [studentsCount, teachersCount, staffsCount, classRoomsCount] = await Promise.all([
+            studentsCountQueryBuilder.getCount(),
+            teachersCountQueryBuilder.getCount(),
+            staffsCountQueryBuilder.getCount(),
+            classRoomsCountQueryBuilder.getRawOne(),
+        ])
 
         return {
             studentsCount,
             teachersCount,
-            classRoomsCount: +classRoomsCount.sectionsCount + +classRoomsCount.primaryWithNoSectionsCount,
             staffsCount,
+            classRoomsCount: +classRoomsCount.sectionsCount + +classRoomsCount.primaryWithNoSectionsCount,
         }
     }
 
     async getLeaveRequests() {
-        const currentAcademicYearId = await this.cacheManager.get(CACHE_KEYS.CAY_ID);
+        const currentAcademicYearId = await this.utilitiesService.getAcademicYearId();
 
         const studentsLeaveRequestsQueryBuilder = this.getRepository(LeaveRequest).createQueryBuilder('leaveRequest')
             .orderBy('leaveRequest.createdAt', 'DESC')
             .leftJoin('leaveRequest.account', 'account')
             .leftJoin('account.student', 'student')
-            .leftJoin('student.enrollments', 'enrollments')
+            .leftJoin('student.enrollments', 'enrollments', 'enrollments.academicYearId = :academicYearId', { academicYearId: currentAcademicYearId })
             .leftJoin('enrollments.classRoom', 'classRoom')
             .leftJoin('classRoom.parent', 'parent')
             .leftJoin('student.profileImage', 'profileImage')
             .where('account.role = :role', { role: Role.STUDENT })
             .andWhere('leaveRequest.status = :status', { status: ELeaveRequestStatus.PENDING })
-            .andWhere('enrollments.academicYearId = :academicYearId', { academicYearId: currentAcademicYearId })
+            .andWhere('enrollments.id IS NOT NULL')
+        this.utilitiesService.applyBranchFilter(studentsLeaveRequestsQueryBuilder);
 
         const studentsLeaveRequests = await studentsLeaveRequestsQueryBuilder.clone()
             .limit(3)
@@ -81,7 +98,6 @@ export class DashboardService extends BaseRepository {
             ]).getRawMany();
 
         const studentsLeaveRequestCount = await studentsLeaveRequestsQueryBuilder.clone().getCount();
-
 
         const teachersLeaveRequestsQueryBuilder = this.getRepository(LeaveRequest).createQueryBuilder('leaveRequest')
             .orderBy('leaveRequest.createdAt', 'DESC')
