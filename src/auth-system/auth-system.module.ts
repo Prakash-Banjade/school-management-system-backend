61831e14e972165c4bc4e25e69bf809304c590ba
Prakash Banjade <prakashbanjade191@gmail.com>
2024-12-31 13:31:39 +0545

    remove unused

--- src/auth-system/auth-system.module.ts
+++ src/auth-system/auth-system.module.ts
@@ -3,8 +3,6 @@ import { UsersModule } from './users/users.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { CaslModule } from './casl/casl.module';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { AbilitiesGuard } from 'src/common/guards/abilities.guard';
 import { JwtModule } from './jwt/jwt.module';
 import { EncryptionModule } from './encryption/encryption.module';
 
@@ -16,12 +14,6 @@ import { EncryptionModule } from './encryption/encryption.module';
         AuthModule,
         JwtModule,
         EncryptionModule,
-    ],
-    providers: [
-        // {
-        //     provide: APP_GUARD,
-        //     useClass: AbilitiesGuard, // global ability guard
-        // },
     ]
 })
 export class AuthSystemModule { }
