aa934a89a3f242d9839d0d96afc981769f6d5974
Prakash Banjade <prakashbanjade191@gmail.com>
2025-01-09 10:42:56 +0545

    feat: restrict reducing copies count than issued count

--- src/library-system/library-book/dto/update-library-book.dto.ts
+++ src/library-system/library-book/dto/update-library-book.dto.ts
@@ -1,10 +1,5 @@
-import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateLibraryBookDto } from './create-library-book.dto';
-import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateLibraryBookDto extends PartialType(CreateLibraryBookDto) {
-    @ApiPropertyOptional({ type: Boolean, description: 'Available or not' })
-    @IsBoolean()
-    @IsOptional()
-    available: boolean
 }
