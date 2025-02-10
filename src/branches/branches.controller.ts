ff68f0ce2cd6ccdcc9e29071832d3f2270cf38a8
Prakash Banjade <prakashbanjade191@gmail.com>
2025-02-10 16:38:45 +0545

    feat: quick fix

--- src/branches/branches.controller.ts
+++ src/branches/branches.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Query } from '@nestjs/common';
 import { BranchesService } from './branches.service';
 import { CreateBranchDto } from './dto/create-branch.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiConflictResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { UpdateBranchDto } from './dto/update-branch.dto';
 import { QueryDto } from 'src/common/dto/query.dto';
 import { CheckAbilities } from 'src/common/decorators/abilities.decorator';
@@ -14,30 +14,45 @@ export class BranchesController {
   constructor(private readonly branchesService: BranchesService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create a branch' })
+  @ApiCreatedResponse({ description: 'Branch created' })
+  @ApiConflictResponse({ description: 'Branch name already exists' })
   @CheckAbilities({ subject: Role.SUPER_ADMIN, action: Action.CREATE })
   create(@Body() createBranchDto: CreateBranchDto) {
     return this.branchesService.create(createBranchDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all branches' })
+  @ApiOkResponse({ description: 'Successfully received branch list' })
   @CheckAbilities({ subject: Role.SUPER_ADMIN, action: Action.READ })
   findAll(@Query() queryDto: QueryDto) {
     return this.branchesService.findAll(queryDto);
   }
 
   @Get('options')
+  @ApiOperation({ summary: 'Get all branches options' })
+  @ApiOkResponse({ description: 'Successfully received branch options' })
   @CheckAbilities({ subject: Role.SUPER_ADMIN, action: Action.READ })
   getOptions(@Query() queryDto: QueryDto) {
     return this.branchesService.getOptions(queryDto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get branch by id', description: 'Get branch by id.' })
+  @ApiNotFoundResponse({ description: 'Branch not found' })
+  @ApiOkResponse({ description: 'Successfully received branch' })
+  @ApiParam({ name: 'id', type: String })
   @CheckAbilities({ subject: Role.SUPER_ADMIN, action: Action.READ })
   findOne(@Param('id') id: string) {
     return this.branchesService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a branch', description: 'Update a branch with the given id.' })
+  @ApiNotFoundResponse({ description: 'Branch not found' })
+  @ApiOkResponse({ description: 'Successfully updated branch' })
+  @ApiParam({ name: 'id', type: String })
   @CheckAbilities({ subject: Role.SUPER_ADMIN, action: Action.UPDATE })
   update(@Param('id') id: string, @Body() updateBranchDto: UpdateBranchDto) {
     return this.branchesService.update(id, updateBranchDto);
