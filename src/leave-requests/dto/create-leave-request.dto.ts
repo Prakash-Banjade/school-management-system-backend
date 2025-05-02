8a0a04ed73769e27e9b32e16b62dbca5fca7cd33
Prakash Banjade <prakashbanjade191@gmail.com>
2025-05-02 16:44:21 +0545

    feat: set leave request to request from today

--- src/leave-requests/dto/create-leave-request.dto.ts
+++ src/leave-requests/dto/create-leave-request.dto.ts
@@ -1,17 +1,17 @@
 import { BadRequestException } from "@nestjs/common";
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
-import { IsDateString, IsNotEmpty, IsOptional, IsString, Length, ValidateIf } from "class-validator";
-import { differenceInDays, isBefore, startOfDay } from "date-fns";
-import { IsFutureDate } from "src/common/decorators/validators/isFutureDate.decorator";
+import { IsDateString, IsNotEmpty, IsString, Length, ValidateIf } from "class-validator";
+import { differenceInDays, isBefore, isToday, startOfDay } from "date-fns";
 
 export class CreateLeaveRequestDto {
     @ApiProperty({ type: "string", format: 'date-time', example: '2022-10-18T00:00:00.000Z', description: 'Leave from date' })
     @IsDateString()
-    @IsFutureDate({ message: 'Leave from date cannot be in the past' })
     @Transform(({ value }) => {
         if (isNaN(Date.parse(value))) throw new BadRequestException('Invalid leave from date');
 
+        if (isBefore(new Date(value), new Date()) && !isToday(new Date(value))) throw new BadRequestException('Leave from date cannot be in the past');
+        
         if (differenceInDays(startOfDay(new Date(value)), startOfDay(new Date())) > 7) throw new BadRequestException('Leave from date cannot be more than 7 days in the future');
 
         return value;
